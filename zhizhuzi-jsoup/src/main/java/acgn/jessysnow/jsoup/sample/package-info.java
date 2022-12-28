fa0b99278548a41b1131e224b213fd83e2a10d89
JessySnow <jessysnow12345@gmail.com>
2022-12-28 14:56:42 +0800

    Modify -- Update file struct

---
+++ zhizhuzi-jsoup/src/main/java/acgn/jessysnow/jsoup/sample/package-info.java
@@ -0,0 +1,4 @@
+/**
+ * Brief info of how to use dom node parser
+ */
+package acgn.jessysnow.jsoup.sample;
---
+++ zhizhuzi-jsoup/src/test/java/acgn/jessysnow/jsoup/parser/JDParseTest.java
@@ -0,0 +1,31 @@
+package acgn.jessysnow.jsoup.parser;
+
+import acgn.jessysnow.jsoup.pojo.WebSite;
+import acgn.jessysnow.jsoup.sample.JDHtml;
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.junit.jupiter.api.Test;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+
+public class JDParseTest {
+    private Parser parser = new DomParser();
+    public static final Document html;
+
+    static {
+        try {
+            File file = new File("src/test/resources/html/JD.html");
+            html = Jsoup.parse(file, StandardCharsets.UTF_8.name());
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    @Test
+    public void test(){
+        JDHtml jd = (JDHtml) parser.parse(html, new JDHtml());
+        System.out.println("SAFDF");
+    }
+}
