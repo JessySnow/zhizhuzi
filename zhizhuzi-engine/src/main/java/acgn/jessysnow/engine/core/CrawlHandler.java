eba7d9cae2e754a04a280303adcc80069bd5332c
JessySnow <jessysnow12345@gmail.com>
2023-01-02 16:55:37 +0800

    Add -- Crawl Result Pipeline support

--- zhizhuzi-engine/src/main/java/acgn/jessysnow/engine/core/CrawlHandler.java
+++ zhizhuzi-engine/src/main/java/acgn/jessysnow/engine/core/CrawlHandler.java
@@ -1,8 +1,12 @@
 package acgn.jessysnow.engine.core;
 
+import acgn.jessysnow.jsoup.pojo.WebSite;
 import io.netty.channel.ChannelHandler;
 import io.netty.channel.ChannelHandlerContext;
 import io.netty.channel.ChannelInboundHandlerAdapter;
+
+import java.util.concurrent.ExecutorService;
+import java.util.function.Consumer;
 
 /**
  * A Netty-Inbound handler work with NettyClientEngine
@@ -10,9 +14,31 @@ import io.netty.channel.ChannelInboundHandlerAdapter;
  * @see NettyClientEngine
  */
 @ChannelHandler.Sharable
-public class CrawlHandler extends ChannelInboundHandlerAdapter {
+public class CrawlHandler<T extends WebSite> extends ChannelInboundHandlerAdapter {
+
+    Class<T> clazz;
+    private final ExecutorService resultPipeline;
+    private final Consumer<T> consumeLogic;
+
+    public CrawlHandler(ExecutorService resultPipeline, Class<T> clazz, Consumer<T> consumeLogic){
+        this.resultPipeline = resultPipeline;
+        this.clazz = clazz;
+        this.consumeLogic = consumeLogic;
+    }
+
+    /**
+     * This handler is placed behind a message2message_encoder, so it will get a Website from pipeline
+     *      - Cause consumeLogic will be a time-consuming operation, submit it to a executor-service
+     * @param msg A Website-pojo
+     */
     @Override
     public void channelRead(ChannelHandlerContext ctx, Object msg) throws Exception {
-        super.channelRead(ctx, msg);
+        T website = (T) msg;
+        resultPipeline.execute(new Runnable() {
+            @Override
+            public void run() {
+                consumeLogic.accept(website);
+            }
+        });
     }
 }
