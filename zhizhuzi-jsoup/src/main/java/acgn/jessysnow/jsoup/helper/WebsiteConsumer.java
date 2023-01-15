c085813854ab329b88d8d50a99df745898cc6af4
JessySnow <jessysnow12345@gmail.com>
2023-01-15 21:10:15 +0800

    Modify -- add helper consumer

---
+++ zhizhuzi-jsoup/src/main/java/acgn/jessysnow/jsoup/helper/WebsiteConsumer.java
@@ -0,0 +1,58 @@
+package acgn.jessysnow.jsoup.helper;
+
+import acgn.jessysnow.jsoup.annotation.Nodes;
+import acgn.jessysnow.jsoup.pojo.WebSite;
+import lombok.extern.log4j.Log4j2;
+
+import java.lang.annotation.Annotation;
+import java.lang.reflect.Field;
+import java.util.List;
+
+/**
+ * Consume Website we crawl
+ */
+@Log4j2
+public class WebsiteConsumer {
+    public static void toConsole(WebSite webSite) throws IllegalAccessException {
+        if (null == webSite){
+            log.error("Can't consume a website which is null");
+            return ;
+        }
+
+        Field[] fields = webSite.getClass().getDeclaredFields();
+        for (Field field : fields){
+            Annotation[] annotations = field.getAnnotations();
+            int index = 0;
+            for (; index < annotations.length; ++ index){
+                if (annotations[index].annotationType().equals(Nodes.class)){
+                    break;
+                }
+            }
+            if(index == annotations.length){
+                continue;
+            }
+
+            field.setAccessible(true);
+            Object o = field.get(webSite);
+        }
+    }
+
+    private void _printIt(Object result, Field field){
+        if (result == null){
+            log.error("{} field is null", field.getName());
+            return ;
+        }
+
+        if (result.getClass().equals(String.class)){
+            String fieldName = field.getName();
+            System.out.printf("%s : %s", fieldName, result);
+        }else if (result instanceof List){
+            System.out.println(field.getName());
+            List<?> list = ((List<?>) result);
+            if (list.size() > 0 && !list.get(0).getClass().equals(String.class)){
+                log.error("{} field's type unsupported", field.getName());
+            }
+            list.forEach(item -> System.out.printf("%s ", item));
+        }
+    }
+}
--- zhizhuzi-jsoup/src/main/java/acgn/jessysnow/jsoup/pojo/WebSite.java
+++ zhizhuzi-jsoup/src/main/java/acgn/jessysnow/jsoup/pojo/WebSite.java
@@ -6,5 +6,9 @@ import acgn.jessysnow.jsoup.sample.SimpleHtml;
  * Tag pojo of html document
  * All field want injected by parser must be annotated by Nodes or Node
  * @see SimpleHtml demo
+ * <br>
+ * Accaptable field type:
+ *      String
+ *      List<String>
  */
 public abstract class WebSite {}
