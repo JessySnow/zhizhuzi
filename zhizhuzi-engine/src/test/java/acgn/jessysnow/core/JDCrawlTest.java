cd58f6edfc0d4e706d8f64cef6f915605a590dc1
JessySnow <jessysnow12345@gmail.com>
2023-02-11 14:06:05 +0800

    Add -- JD Url datasku Pojo

---
+++ zhizhuzi-engine/src/test/java/acgn/jessysnow/core/JDCrawlTest.java
@@ -0,0 +1,40 @@
+package acgn.jessysnow.core;
+
+import acgn.jessysnow.engine.core.NettyClientEngine;
+import acgn.jessysnow.engine.pojo.CrawlTask;
+import acgn.jessysnow.jsoup.helper.WebsiteConsumer;
+import acgn.jessysnow.jsoup.sample.JDItemSkus;
+import acgn.jessysnow.jsoup.sample.JDUrlSkus;
+import io.netty.handler.codec.http.HttpMethod;
+import io.netty.handler.codec.http.HttpVersion;
+import org.junit.jupiter.api.Test;
+
+import java.net.URI;
+
+public class JDCrawlTest {
+    @Test
+    public void test_urlList(){
+        try(NettyClientEngine nettyClientEngine = new NettyClientEngine.NettyEngineBuilder().getCrawlEngine(
+                true, true, null, WebsiteConsumer::toConsole,
+                JDUrlSkus.class)){
+            nettyClientEngine.execute(new CrawlTask("search.jd.com", 443,
+                    new URI("https://search.jd.com/Search?keyword=分形工艺"),
+                    HttpVersion.HTTP_1_1, HttpMethod.GET
+                    ,null, null));
+            Thread.sleep(5000);
+        }catch (Exception ignored){;}
+    }
+
+    @Test
+    public void test_itemList(){
+        try(NettyClientEngine nettyClientEngine = new NettyClientEngine.NettyEngineBuilder().getCrawlEngine(
+                true, true, null, WebsiteConsumer::toConsole,
+                JDItemSkus.class)){
+            nettyClientEngine.execute(new CrawlTask("item.jd.com", 443,
+                    new URI("https://item.jd.com/100048428239.html"),
+                    HttpVersion.HTTP_1_1, HttpMethod.GET
+                    ,null, null));
+            Thread.sleep(5000);
+        }catch (Exception ignored){;}
+    }
+}
---
+++ zhizhuzi-jsoup/src/main/java/acgn/jessysnow/jsoup/sample/JDUrlSkus.java
@@ -0,0 +1,26 @@
+package acgn.jessysnow.jsoup.sample;
+
+import acgn.jessysnow.jsoup.annotation.Node;
+import acgn.jessysnow.jsoup.annotation.Nodes;
+import acgn.jessysnow.jsoup.enums.NodeTagName;
+import acgn.jessysnow.jsoup.pojo.WebSite;
+import lombok.Getter;
+
+import java.util.List;
+
+public class JDUrlSkus extends WebSite {
+    @Nodes(domNodes = {
+            @Node(nodeTagName = NodeTagName.body),
+            @Node(nodeId = "J_searchWrap"),
+            @Node(nodeId = "J_container"),
+            @Node(nodeId = "J_main"),
+            @Node(nodeClassName = "m-list"),
+            @Node(nodeClassName = "ml-wrap"),
+            @Node(nodeId = "J_goodsList"),
+            @Node(nodeClassName = "gl-warp clearfix"),
+            @Node(nodeTagName = NodeTagName.li),
+            @Node(nodeAttr = "data-sku")
+    })
+    @Getter
+    private List<String> urlSkus;
+}
