64dd45906027a523443a0343cf2c03a445643bb7
JessySnow <jessysnow12345@gmail.com>
2023-02-13 14:33:45 +0800

    Fixme

--- zhizhuzi-engine/src/main/java/acgn/jessysnow/engine/core/WebSiteConverter.java
+++ zhizhuzi-engine/src/main/java/acgn/jessysnow/engine/core/WebSiteConverter.java
@@ -1,9 +1,7 @@
 package acgn.jessysnow.engine.core;
 
-import acgn.jessysnow.gson.pojo.Json;
 import acgn.jessysnow.jsoup.parser.DomParser;
 import acgn.jessysnow.jsoup.pojo.WebSite;
-import com.google.gson.Gson;
 import io.netty.channel.ChannelHandlerContext;
 import io.netty.handler.codec.MessageToMessageDecoder;
 import io.netty.handler.codec.http.HttpContent;
@@ -23,21 +21,22 @@ public class WebSiteConverter<T extends WebSite> extends MessageToMessageDecoder
         this.clazz = clazz;
     }
 
+    // Fixme
     @Override
     protected void decode(ChannelHandlerContext ctx, HttpContent msg, List<Object> out) throws Exception {
         String response = msg.content().toString(CharsetUtil.UTF_8);
 
         T res;
-        if (clazz.isAssignableFrom(Json.class)){
-            Gson gson = new Gson();
-            res = gson.fromJson(response, clazz);
-        }else if (clazz.isAssignableFrom(WebSite.class)) {
+//        if (clazz.isAssignableFrom(Json.class)){
+//            Gson gson = new Gson();
+//            res = gson.fromJson(response, clazz);
+//        }else if (clazz.isAssignableFrom(WebSite.class)) {
             Document document = Jsoup.parse(response);
             DomParser<T> parser = new DomParser<>();
             res = parser.parse(document, clazz.getConstructor().newInstance());
-        }else {
-            res = null;
-        }
+//        }else {
+//            res = null;
+//        }
 
         out.add(res);
     }
