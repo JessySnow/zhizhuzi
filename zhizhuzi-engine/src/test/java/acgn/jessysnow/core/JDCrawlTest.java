8ce0f6d7e7afcb03dccba929d15ef3ff929ca33e
JessySnow <jessysnow12345@gmail.com>
2023-02-18 22:24:08 +0800

    Modify -- simplify api

--- zhizhuzi-engine/src/test/java/acgn/jessysnow/core/JDCrawlTest.java
+++ zhizhuzi-engine/src/test/java/acgn/jessysnow/core/JDCrawlTest.java
@@ -5,11 +5,7 @@ import acgn.jessysnow.engine.core.NettyClientEngine;
 import acgn.jessysnow.engine.pojo.CrawlTask;
 import acgn.jessysnow.jsoup.helper.WebsiteConsumer;
 import acgn.jessysnow.jsoup.sample.JDUrlSkus;
-import io.netty.handler.codec.http.HttpMethod;
-import io.netty.handler.codec.http.HttpVersion;
 import org.junit.jupiter.api.Test;
-
-import java.net.URI;
 
 // Crawl item data-sku from search.jd.com
 public class JDCrawlTest {
@@ -18,10 +14,7 @@ public class JDCrawlTest {
         try(NettyClientEngine<JDUrlSkus> engine = new EngineBuilder<JDUrlSkus>(JDUrlSkus.class).ssl(true)
                     .compress(true).resConsumer(WebsiteConsumer::toConsole).build();
         ){
-            engine.blockExecute(new CrawlTask("search.jd.com", 443,
-                    new URI("https://search.jd.com/Search?keyword=分形工艺"),
-                    HttpVersion.HTTP_1_1, HttpMethod.GET
-                    ,null, null));
+            engine.blockExecute(new CrawlTask("https://search.jd.com/Search?keyword=分形工艺"));
         }catch (Exception ignored){;}
     }
 }
