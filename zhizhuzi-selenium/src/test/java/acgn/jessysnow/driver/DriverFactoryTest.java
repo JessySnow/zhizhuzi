50e8a007922564ba0e04a0e20f6ac09a753be01e
JessySnow <jessysnow12345@gmail.com>
2023-03-20 19:51:40 +0800

    Modify -- Update Test case

--- zhizhuzi-selenium/src/test/java/acgn/jessysnow/driver/DriverFactoryTest.java
+++ zhizhuzi-selenium/src/test/java/acgn/jessysnow/driver/DriverFactoryTest.java
@@ -4,12 +4,36 @@ import acgn.jessysnow.enums.Browsers;
 import org.junit.Test;
 import org.openqa.selenium.WebDriver;
 
-import static org.junit.Assert.*;
-
 public class DriverFactoryTest {
     @Test
-    public void testDriverGetAndRelease(){
-        WebDriver driver = DriverFactory.buildDriver(Browsers.Edge);
-        DriverFactory.releaseDriver(driver);
+    public void testDriverGetAndRelease() throws InterruptedException {
+        Thread[] threads = buildThread(5, new Runnable() {
+            @Override
+            public void run() {
+                WebDriver driver = DriverFactory.buildDriver(Browsers.Edge);
+                doSomethingWithDriver(driver);
+            }
+        });
+
+        for (Thread thread : threads) {
+            thread.start();
+        }
+        Thread.sleep(4096 * 6);
+    }
+
+    private void doSomethingWithDriver(WebDriver driver){
+        try {
+            System.out.println(driver);
+            Thread.sleep(4096);
+            DriverFactory.releaseDriver(driver);
+        } catch (InterruptedException ignored) {}
+    }
+
+    private Thread[] buildThread(int size, Runnable runnable){
+        Thread[] threads = new Thread[size];
+        for (int i = 0; i < size; i++){
+            threads[i] = new Thread(runnable);
+        }
+        return threads;
     }
 }
